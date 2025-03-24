5aace8d0d60fbfcf6da561de60344952cab76fb4
williamgl3 <159193524+williamgl3@users.noreply.github.com>
2025-03-23 21:09:42 -0600

    encabezadoPG

---
+++ src/components/encabezado.vue
@@ -0,0 +1,38 @@
+<template>
+<nav class="navbar navbar-expand-lg" data-bs-theme="dark" style="background: url('https://www.shutterstock.com/image-photo/motorcycle-parked-alone-on-sunny-260nw-2153623869.jpg') no-repeat center center; background-size: cover; padding: 15px;">
+            <div class="container-fluid" style="background: rgba(2, 2, 2, 0.5); padding: 10px; border-radius: 10px;">
+                <a class="navbar-brand text-white" href="#">Motos Deportivas</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#">
+                                <i class="bi bi-house-fill"></i> Inicio
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#objetivo">
+                                <i class="bi bi-bullseye"></i> Objetivo
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#tipos">
+                                <i class="bi bi-lightning-fill"></i> Tipos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#historia">
+                                <i class="bi bi-book-half"></i> Historia
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+        </nav>
+
+
+</template>
