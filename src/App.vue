69b6df8288038798494c5975749010f90c0152e1
williamgl3 <159193524+williamgl3@users.noreply.github.com>
2025-03-26 13:33:35 -0600

    creacion de los props y eventos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
-import Principal from './components/Principal.vue';
-import vFor from './components/v-for.vue';
-import recorrido from './components/recorrido.vue';
-import horario from './components/horario.vue';
+// import Principal from './components/Principal.vue';
+// import vFor from './components/v-for.vue';
+// import recorrido from './components/recorrido.vue';
+// import horario from './components/horario.vue';
+// import padre from './components/props/padre.vue';
 </script>
 
 <template>
-  
-  <principal nombre="Eddi William Gonzalez Leon" />
+  <!-- <principal nombre="Eddi William Gonzalez Leon" /> -->
    <!-- <recorrido/>
    <horario/> -->
+<!-- <padre/> -->
+ 
 </template>
 
 <style scoped>
--- src/components/props/padre.vue
+++ src/components/props/padre.vue
@@ -1,12 +1,39 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref } from 'vue';
+import hijo1 from './hijo1.vue';
+import hijo2 from './hijo2.vue';
 defineProps({
-nombre: String
-})
-
+  nombre: String
+});
 </script>
+
 <template>
-<h1>{{ nombre }}</h1>
+  <div class="container text-center mt-4">
+    <h1 class="text-danger fw-bold display-4" style="font-family: 'Arial Black', sans-serif;">
+      <i class="fas fa-user"></i> Soy el Componente Padre
+    </h1>
+    <div class="row justify-content-between mb-5">
+      <div class="col-md-5 mb-5">
+        <div class="row align-items-center bg-warning p-4 rounded shadow">
+          <i class="fas fa-child fa-2x me-3 text-dark"></i>
+          <hijo1 class="mt-4 p-3 border rounded bg-white" edad="10" />
+        </div>
+      </div>
+      <div class="col-md-5 mb-5">
+        <div class="row align-items-center bg-info p-4 rounded shadow">
+          <i class="fas fa-child fa-2x me-3 text-dark"></i>
+          <hijo2 class="mt-4 p-3 border rounded bg-white" edad="4" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+.container {
+  max-width: 800px;
+}
+h1 {
+  font-family: 'Arial Black', sans-serif;
+}
+</style>
