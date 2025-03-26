0d56281080319dc4bbd807c1c6bbc0558a04c524
williamgl3 <159193524+williamgl3@users.noreply.github.com>
2025-03-26 13:54:02 -0600

    uso de eventos con data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import Eventos from './components/Eventos/eventos.vue';
+
 // import Principal from './components/Principal.vue';
 // import vFor from './components/v-for.vue';
 // import recorrido from './components/recorrido.vue';
 // import horario from './components/horario.vue';
 // import padre from './components/props/padre.vue';
+import eventos from './components/Eventos/eventos.vue';
 </script>
 
 <template>
@@ -11,7 +14,7 @@
    <!-- <recorrido/>
    <horario/> -->
 <!-- <padre/> -->
- 
+<eventos/>
 </template>
 
 <style scoped>
--- src/components/Eventos/eventos.vue
+++ src/components/Eventos/eventos.vue
@@ -1,13 +1,44 @@
-<template lang="">
-    <div>
-        
+<template >
+    <div class="container">
+        <div class="row mt-4">
+            <div>
+                <h4>Evento Click</h4>
+                <button @click="clic" class="btn btn-danger" type="button">Haga clic</button>
+                <button @click="soyboton" class="btn btn-warning mx-2" type="button">Haga clic2</button>
+            </div>
+            <div>
+                <h4>Evento Change</h4>
+                <label class="input-group-text" for="">Ingresa tu Nombre</label>
+                <input class="from-control mt-2" type="text" id="nombre" @change="nombreE">
+                <strong>el nombre ingresado es: {{ nombre }}</strong>
+            </div>
+           
+        </div>
+
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return{
+            nombre: ''
+        }
+    },
+    methods: {
+        clic(){
+            alert("gracias por darle clic")
+        },
+        soyboton(){
+            alert("soy un boton")
+        },
+        nombreE(event){
+            this.nombre = event.target.value
+        }
+
+    },
     
 }
 </script>
-<style lang="">
+<style >
     
 </style>
